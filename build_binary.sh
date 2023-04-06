128c115d0fe031167d296f7e2de6e9edd5bcd1e8
Dan S. Camper <dan@bti.net>
2023-04-06 07:38:16 -0500

    Unsilence quicklisp loads and builds
    Signed-off-by: Dan S. Camper <dan@bti.net>

--- build_binary.sh
+++ build_binary.sh
@@ -13,7 +13,7 @@ mkdir -p bin
 buildapp --output bin/xml2ecl \
   --eval "(let* ((home-dir (user-homedir-pathname)) (setup (or (probe-file (merge-pathnames \"quicklisp/setup.lisp\" home-dir)) (probe-file (merge-pathnames \"portacle/all/quicklisp/setup.lisp\" home-dir))))) (load setup))" \
   --eval "(require :asdf)" \
-  --eval "(ql:quickload '(:adopt :flexi-streams :fxml :with-user-abort) :silent t)" \
+  --eval "(ql:quickload '(:adopt :flexi-streams :fxml :with-user-abort))" \
   --asdf-path . \
   --load-system xml2ecl \
   --entry xml2ecl:toplevel \
